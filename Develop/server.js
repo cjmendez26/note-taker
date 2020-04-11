bc5f735121309370492c87a1d68d0260f05e1919
Jane <cjmendez26@gmail.com>
2020-04-11 13:51:39 -0700

    comment

--- Develop/server.js
+++ Develop/server.js
@@ -18,7 +18,8 @@ app.use(express.json());
 
 
 
-
+// Starts the server to begin listening
+// =============================================================
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });

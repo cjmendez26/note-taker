f93a01a5b14efadd9c4e9a36c1a60c5afc2631dc
Jane <cjmendez26@gmail.com>
2020-04-14 17:55:05 -0700

    code fix

--- Develop/server.js
+++ Develop/server.js
@@ -34,19 +34,24 @@ app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-app.post("/api/notes", function(req, res){
+app.post("/api/notes", function (req, res) {
     var newNote = req.body;
-
-    // take what you have already in the file and add to it, don't overwrite what's already there
-
-    // use JSON.stringify
-
-    fs.writeFile(__dirname + "/db/db.json", newNote, function(){
-        console.log("testing function");
-    });
-    
+    newNote.id = Date.now();
+    console.log(newNote);
+    fs.readFile(__dirname + "/db/db.json", function (err, data) {
+        if (err) throw err;
+        let notes = JSON.parse(data);
+        // console.log(notes);
+        notes.push(newNote);
+        fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), function () {
+            console.log("testing function");
+        });
+    })
 });
-
+app.delete("/api/notes/:id", function(req, res){
+    console.log(req.params.id);
+    // read the file use loop to get the note by id delete it  
+})
 
 // Starts the server to begin listening
 // =============================================================
